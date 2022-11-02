f95fc78b86fd5258f07a338ab2f6fa5ca87fc7e6
Stop__Internets <69037662+StopInternet@users.noreply.github.com>
2022-11-02 15:01:44 +0900

    Add files via upload

--- class_support_app/urls.py
+++ class_support_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'class_support_app'
 urlpatterns = [
     path('',views.IndexView.as_view(),name="index"),
+    path('timer',views.TimerMainView.as_view(),name="timer"),
+    path('timer_kentei',views.TimerDentakuView.as_view(),name="timer_kentei"),
+    path('timer_aocc',views.TimerAoccView.as_view(),name="timer_aocc")
 ]
--- class_support_app/views.py
+++ class_support_app/views.py
@@ -2,3 +2,12 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
+
+class TimerMainView(generic.TemplateView):
+    template_name= "timer.html"
+
+class TimerDentakuView(generic.TemplateView):
+    template_name = "timer_kentei.html"
+
+class TimerAoccView(generic.TemplateView):
+    template_name = "timer_aocc.html"
