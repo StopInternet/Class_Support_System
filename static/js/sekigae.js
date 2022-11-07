4c4fc1014125233adcc0740acf76ae763c0f5163
Stop__Internets <69037662+StopInternet@users.noreply.github.com>
2022-11-07 15:41:40 +0900

    Add files via upload

--- static/js/sekigae.js
+++ static/js/sekigae.js
@@ -1,32 +1,36 @@
-// div要素を格納する配列
-divs = [];
+//定義
+var classMem = 0;
 
-window.onload = function(){
-    
-    var parent = document.getElementById('parent');
-
-    //div要素を追加
-    for (i = 0; i < 5; i++){
-        var div = document.createElement('div');
-        div.index = i;
-        div.textContent = 'No. ' + i;
-        div.className = 'child';
-        // click時
-        div.onclick = myClick;
-        parent.appendChild(div);
-        // 配列に追加
-        divs.push(div); 
+//データ配列
+const app = newVue({
+    el:'#app',
+    data:{
+        name:[
+        ]
     }
+})
+
+const Mess = {
+    data(){
+    return{
+        message:0
+    }
+    },
+    mounted(){
+        setInterval(() =>{
+            this.counter++
+        },1000)
+    },
+    delimiters:['[[',']]']
 }
 
-// クリック時の処理
-function myClick(e){
-    
-    // クリックされた要素のindex
-    var i = e.target.index;
-    console.log(i);
-    
-    // 配列divsを利用してtextContentを取得
-    var str = divs[i].textContent;
-    console.log((str)); 
+Vue.createApp(Mess).mount("#two-way-binding")
+
+function MasGene(){
+    classMem = "tate"+"yoko"
+    for(const i = 1;i<=classMem;i++){
+        var div = document.createElement('div');
+        div.className = 'sek';
+        document.getElementsById('wrapper').appendChild(div);
+    }
 }
