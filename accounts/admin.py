e75d1ca46f331e0a47db1279daa38f6e59dbf976
Stop__Internets <69037662+StopInternet@users.noreply.github.com>
2022-11-25 15:44:10 +0900

    Add files via upload

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import CustomUser
+from import_export.admin import ImportExportMixin
+from import_export import resources
+
 # Register your models here.
 admin.site.register(CustomUser)
+
+class UserResource(resources.ModelResource):
+  class Meta:
+    model = CustomUser
+
+@admin.register(CustomUser)
+class UserAdmin(ImportExportMixin,admin.ModelAdmin):
+  resource_class = UserResource
