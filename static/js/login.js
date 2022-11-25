fe18264dcaf96d46f66a5fdf444509f6212cbe9d
Stop__Internets <69037662+StopInternet@users.noreply.github.com>
2022-11-25 15:47:17 +0900

    Add files via upload

--- static/js/login.js
+++ static/js/login.js
@@ -1,16 +1,14 @@
-function login_targets(){
-  //select boxから値を取得
-  const lt = document.form1.login_target;
-	const num = lt.selectedIndex;
-	const str = lt.options[num].value;
-
-  //既に入力画面がある場合初期化
-  document.getElementById('login_menu').innerHTML = " ";
-
-  //値で分岐
-  if(str == "student"){
-    document.getElementById('login_menu').innerHTML = '<br>クラスID:<input type="text">'
-  }else if(str == "teacher"){
-
-  }
+function login_students(){
+    document.getElementById('login_menu').innerHTML = 
+    '<br>クラスID:<br><input type="text">'+
+    '<br>生徒番号:<br><input type="text">'+
+    '<br><hr><input type="button" id="jump_login" value="ログイン" onclick="login_student()">'
 }
+
+function login_teachers(){
+  document.getElementById('login_menu').innerHTML = 
+  '<br>教員ID:<br><input type="text">'+
+  '<br>パスワード:<br><input type="text">'+
+  '<br><hr><input type="button" id="jump_login" value="ログイン" onclick="login_teacher()">'
+}
+
